5853351275777c81fed00624496821269bb6ef80
Nicholas von Waltsleben <nic@nicbook.local>
2017-04-11 15:52:36 +0200

    Refactored to use unittest

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,29 +1,40 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
 
-# Edith has heard about a cool new online to-do app.  She goes
-# to check out its homepage
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-# She notices the page title and header mention to-do lists
-assert 'To-Do' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-# She is invited to enter a to-do item straight away
+    def tearDown(self):
+        self.browser.quit()
 
-# She types "Buy peacock feathers" into a text box (Edith's hobby
-# is tying fly-fishing lures)
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app.  She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
 
-# When she hits enter, the page updates, and now the page lists
-# "1: Buy peacock feathers" as an item in a to-do list
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
 
-# There is still a text box inviting her to add another item.  She
-# enters "Use peacock feathers to make a fly" (Edith is very methodical)
+        # She is invited to enter a to-do item straight away
 
-# The page updates again, and now shows both items on her lists
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
 
-# She visits that URL - her to-do list is still there.
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list
 
-# Satisfied, she goes back to sleep
+        # There is still a text box inviting her to add another item.  She
+        # enters "Use peacock feathers to make a fly" (Edith is very methodical)
 
-browser.quit()
+        # The page updates again, and now shows both items on her lists
+
+        # She visits that URL - her to-do list is still there.
+
+        # Satisfied, she goes back to sleep
+
+if __name__ == '__main__':
+    unittest.main()
